31092490a9002a4dde3851727ed09933b251bea1
Konce Quispe <konceq@fb.com>
2018-06-28 13:18:10 -0700

    Added GoLiveActivity page

---
+++ app/src/main/java/com/cloningapp/konceq/cloningapp/GoLiveActivity.java
@@ -0,0 +1,13 @@
+package com.cloningapp.konceq.cloningapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoLiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_live);
+    }
+}
